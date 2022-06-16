248d5b5a91cdadf5b036ebd155775504a58c6de7
Tomas Kubica <tkubica12@gmail.com>
2022-06-16 08:07:27 +0200

    fix Go code

--- d-aks-devexperience/web.go
+++ d-aks-devexperience/web.go
@@ -2,16 +2,13 @@ package main
 
 import (
    "github.com/go-martini/martini"
-   "github.com/satori/go.uuid"
+   "github.com/google/uuid"
 )
 
 func main() {
   m := martini.Classic()
 
-  id, err := uuid.NewV4()
-  if err != nil {
-    panic("Unable to generate uuid")
-  }
+  id := uuid.New()
 
   m.Get("/", func() string {
     return "Version 1: server id " + id.String()
