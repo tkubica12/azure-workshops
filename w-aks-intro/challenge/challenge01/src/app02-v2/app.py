340b313f87819200ae847de8e05ff6f592622d14
Tomas Kubica <tkubica12@gmail.com>
2023-02-23 07:04:52 +0100

    ch01 src

---
+++ w-aks-intro/challenge/challenge01/src/app02-v2/app.py
@@ -0,0 +1,9 @@
+# Simple Python Flask web app
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<H1>Hello from Python v2!</H1>"
+
+app.run(host='0.0.0.0', port=8080)
