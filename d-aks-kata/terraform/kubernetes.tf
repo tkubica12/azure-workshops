49411e737c722c3de2531d3340b36f89ed406e80
Tomas Kubica <tkubica12@gmail.com>
2023-02-25 16:38:54 +0100

    namespace

--- d-aks-kata/terraform/kubernetes.tf
+++ d-aks-kata/terraform/kubernetes.tf
@@ -103,46 +103,45 @@ resource "azapi_resource" "flux_extension" {
   })
 }
 
-# resource "azapi_resource" "flux_config" {
-#   count                     = var.aks_count
-#   type                      = "Microsoft.KubernetesConfiguration/fluxConfigurations@2022-03-01"
-#   name                      = "myconfig"
-#   parent_id                 = azurerm_kubernetes_cluster.main[count.index].id
-#   schema_validation_enabled = false
+resource "azapi_resource" "flux_config" {
+  type                      = "Microsoft.KubernetesConfiguration/fluxConfigurations@2022-03-01"
+  name                      = "mydemo"
+  parent_id                 = azapi_resource.aks.id
+  schema_validation_enabled = false
 
-#   depends_on = [
-#     azapi_resource.flux_extension
-#   ]
+  depends_on = [
+    azapi_resource.flux_extension
+  ]
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
+  identity {
+    type = "SystemAssigned"
+  }
 
-#   body = jsonencode({
-#     properties = {
-#       scope      = "cluster"
-#       namespace  = "flux-system"
-#       sourceKind = "GitRepository"
-#       kustomizations = {
-#         myapp = {
-#           name                   = "myapp"
-#           path                   = "./myapp"
-#           force                  = true
-#           prune                  = true
-#           timeoutInSeconds       = 120
-#           intervalInSeconds      = 60
-#           retryIntervalInSeconds = 60
-#         }
-#       }
-#       gitRepository = {
-#         url                   = "https://github.com/tkubica12/myapp-flux"
-#         timeoutInSeconds      = 120
-#         syncIntervalInSeconds = 60
-#         repositoryRef = {
-#           branch = "main"
-#         }
-#       }
-#     }
-#   })
-# }
+  body = jsonencode({
+    properties = {
+      scope      = "cluster"
+      namespace  = "flux-system"
+      sourceKind = "GitRepository"
+      kustomizations = {
+        myapp = {
+          name                   = "mydemo"
+          path                   = "./d-aks-kata/kubernetes"
+          force                  = true
+          prune                  = true
+          timeoutInSeconds       = 120
+          intervalInSeconds      = 120
+          retryIntervalInSeconds = 120
+        }
+      }
+      gitRepository = {
+        url                   = "https://github.com/tkubica12/azure-workshops"
+        timeoutInSeconds      = 120
+        syncIntervalInSeconds = 120
+        repositoryRef = {
+          branch = "d-aks-kata"
+        }
+      }
+    }
+  })
+}
 
