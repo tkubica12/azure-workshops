ae4a0687ec6a60a0b8820565417872c4ac0b8e78
Tomas Kubica <tkubica12@gmail.com>
2025-05-19 18:20:13 +0200

    Update Dockerfile and Terraform - added container app configurations

---
+++ d-ai-app-patterns/scalable_chat/terraform/container_apps.front_service.tf
@@ -0,0 +1,70 @@
+resource "azurerm_container_app" "front_service" {
+  name                         = "ca-frontservice-${local.base_name}"
+  container_app_environment_id = azurerm_container_app_environment.main.id
+  resource_group_name          = azurerm_resource_group.main.name
+  revision_mode                = "Single"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 8000 
+    transport        = "http"
+
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
+
+  template {
+    min_replicas = 1
+    max_replicas = 5
+
+    container {
+      name   = "front-service"
+      image  = "ghcr.io/tkubica12/azure-workshops/d-ai-app-patterns-scalable-chat-front-service:latest" 
+      cpu    = 0.5
+      memory = "1.0Gi"
+
+      env {
+        name  = "SERVICEBUS_FULLY_QUALIFIED_NAMESPACE"
+        value = azurerm_servicebus_namespace.main.endpoint
+      }
+      env {
+        name  = "SERVICEBUS_USER_MESSAGES_TOPIC"
+        value = "user-messages" 
+      }
+      env {
+        name  = "SERVICEBUS_TOKEN_STREAMS_TOPIC"
+        value = "token-streams" 
+      }
+      env {
+        name  = "SERVICEBUS_TOKEN_STREAMS_SUBSCRIPTION"
+        value = "front-service" 
+      }
+      env {
+        name  = "SERVICEBUS_SENDER_POOL_SIZE"
+        value = "10" 
+      }
+      env {
+        name  = "LOG_LEVEL"
+        value = "INFO"
+      }
+      env {
+        name  = "CORS_ORIGINS"
+        value = "*" 
+      }
+      env {
+        name  = "APPLICATIONINSIGHTS_CONNECTION_STRING"
+        value = azurerm_application_insights.main.connection_string
+      }
+      env {
+        name  = "OTEL_SERVICE_NAME"
+        value = "front-service" 
+      }
+    }
+  }
+}
---
+++ d-ai-app-patterns/scalable_chat/terraform/container_apps.web_client.tf
@@ -0,0 +1,34 @@
+resource "azurerm_container_app" "web_client" {
+  name                         = "ca-webclient-${local.base_name}"
+  container_app_environment_id = azurerm_container_app_environment.main.id
+  resource_group_name          = azurerm_resource_group.main.name
+  revision_mode                = "Single"
+
+  ingress {
+    external_enabled = true
+    target_port      = 80 
+    transport        = "http" 
+
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
+
+  template {
+    min_replicas = 0
+    max_replicas = 1 // Adjust as needed
+
+    container {
+      name   = "web-client"
+      image  = "ghcr.io/tkubica12/azure-workshops/d-ai-app-patterns-scalable-chat-web-client:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      env {
+        name  = "VITE_API_URL"
+        value = "https://${azurerm_container_app.front_service.ingress[0].fqdn}"
+      }
+    }
+  }
+}
