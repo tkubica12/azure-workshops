01ccd355d5212718ba1fac8558c1e9ee6adb085e
Tomas Kubica <tkubica12@gmail.com>
2025-06-06 20:58:51 +0200

    chat history api and ui

--- d-ai-app-patterns/scalable_chat/src/web_client/public/env.js
+++ d-ai-app-patterns/scalable_chat/src/web_client/public/env.js
@@ -2,5 +2,6 @@
 window._env_ = {
   // In production, this file is overwritten by the Docker entrypoint to set runtime API_URL
   API_URL: "http://localhost:8000",
-  SSE_URL: "http://localhost:8002"
+  SSE_URL: "http://localhost:8002",
+  HISTORY_API_URL: "http://localhost:8005"
 };
